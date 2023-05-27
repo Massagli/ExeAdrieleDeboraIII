5f1ca25535ea4b79d5a223930e410848ac7904e7
Massagli <adrielmassagli@gmail.com>
2023-05-26 23:08:35 -0300

    Final step

--- ExeAdrieleDeboraIII/ExeAdrieleDeboraIII/Controllers/HomeController.cs
+++ ExeAdrieleDeboraIII/ExeAdrieleDeboraIII/Controllers/HomeController.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using ExeAdrieleDeboraIII.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+
+
 
 namespace ExeAdrieleDeboraIII.Controllers
 {
@@ -32,6 +35,30 @@ namespace ExeAdrieleDeboraIII.Controllers
             ViewBag.Message = "Página Inicial";
 
             return View();
+        }
+
+        public ActionResult Nota() 
+        {
+            ViewBag.Titulo = "Calculando minha nota";
+            Notas objnotas = new Notas();  
+
+            return View(objnotas);
+        }
+
+        [HttpPost]
+        public ActionResult Resultado(FormCollection objnotas)
+        {
+            ViewBag.Titulo = "CalculaNota";
+            ViewBag.Numero = objnotas["Valor"];
+
+            if (ViewBag.Numero == "")
+            {
+                return RedirectToAction("Nota", "Home");
+            }
+
+            int notas = int.Parse(ViewBag.Numero);
+            ViewBag.Resultado = (10 - notas) ;
+            return  View();
         }
     }
 }
---
+++ ExeAdrieleDeboraIII/ExeAdrieleDeboraIII/Models/Notas.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExeAdrieleDeboraIII.Models
+{
+    public class Notas
+    {
+        public int Valor { get; set; }
+    }
+}
